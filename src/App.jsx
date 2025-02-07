320676f4df340c7dac0556f0dca9a79d95395531
1t1sCooL <mmalabugin@gmail.com>
2025-02-07 11:54:26 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={()=>setCount(count-1)}>- Минус</button>
+        <button className="plus" onClick={()=>setCount(count+1)}>Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
